85306a92d484eb880a9ce94e3c3a3e21cc3ad267
cere2393 <carlos.ramirez@tecnologicoliceocristiano.edu.ec>
2024-08-05 20:04:52 -0500

    Add files via upload

---
+++ notas.py
@@ -0,0 +1,17 @@
+nota1=int(input("ingrese nota1:"))
+nota2=int(input("ingrese nota2:"))
+nota3=int(input("ingrese nota3:"))
+nota4=int(input("ingrese nota4:"))
+media= ((nota1+nota2+nota3+nota4)/4)
+
+if media>89:
+    print("A")
+elif media>79 and media< 90:
+    print("B")
+elif media>69 and media< 80:
+    print("C")
+elif media>59 and media< 70:
+    print("D")
+else:
+    print("E")
+
